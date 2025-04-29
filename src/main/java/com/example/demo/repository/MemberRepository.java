d1ab7102136d5a5696da910b9c65a3112db7b21c
yunjiho <yoonwlgh12@gmail.com>
2025-04-29 23:03:27 +0900

    컴포넌트 스캔 실습

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import com.example.demo.domain.Member;
 import java.util.List;
 import java.util.Optional;
 
+
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
