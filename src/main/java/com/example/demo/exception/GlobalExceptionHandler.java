280785cdfbab9b589572c7b90485c27869328af1
yunjiho <yoonwlgh12@gmail.com>
2025-05-08 16:59:18 +0900

    예외처리 추가

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,4 +1,15 @@
 package com.example.demo.exception;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalStateException.class)
+    public String handleIllegalStateException(IllegalStateException e, Model model){
+        model.addAttribute("error", e.getMessage());
+        return "error/customError";
+    }
 }
