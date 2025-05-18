736bd6d517bcf0efc3d57093702a5362e70b0b5d
yunjiho <yoonwlgh12@gmail.com>
2025-05-18 19:18:30 +0900

    JdbcTemplate 실습

--- src/main/java/com/example/demo/config/SpringConfig.java
+++ src/main/java/com/example/demo/config/SpringConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import com.example.demo.repository.JdbcMemberRepository;
+import com.example.demo.repository.JdbcTemplateMemberRepository;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.service.MemberService;
 import org.springframework.context.annotation.Bean;
@@ -25,6 +26,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
