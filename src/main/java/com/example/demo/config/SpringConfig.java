706d128e9b678581651893bdf4951ac2bd367979
yunjiho <yoonwlgh12@gmail.com>
2025-05-18 18:24:59 +0900

    import 삭제

--- src/main/java/com/example/demo/config/SpringConfig.java
+++ src/main/java/com/example/demo/config/SpringConfig.java
@@ -3,7 +3,6 @@ package com.example.demo.config;
 import com.example.demo.repository.JdbcMemberRepository;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
