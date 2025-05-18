bd8808ba55003933ba111fb1ca445d9b5d0a37fc
yunjiho <yoonwlgh12@gmail.com>
2025-05-18 18:56:12 +0900

    스프링 통합 테스트

--- src/test/java/com/example/demo/service/MemberServiceIntegrationTest.java
+++ src/test/java/com/example/demo/service/MemberServiceIntegrationTest.java
@@ -1,28 +1,27 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.Member;
-import com.example.demo.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.*;
+import com.example.demo.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class MemberServiceTest {
+@SpringBootTest
+@Transactional
+public class MemberServiceIntegrationTest {
 
+    @Autowired
     MemberService memberService;
-    MemoryMemberRepository memberRepository;
 
-    @BeforeEach
-    public void beforeEach(){
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-
-    @AfterEach
-    public void afterEach(){
-        memberRepository.storeClear();
-    }
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     @DisplayName("join 테스트")
