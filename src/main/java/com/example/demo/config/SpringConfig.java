b6f3a78fb71024192022eb0528fae83254371cc9
yunjiho <yoonwlgh12@gmail.com>
2025-05-18 18:23:36 +0900

    configuration을 수정하여 h2 사용하게 변경

--- src/main/java/com/example/demo/config/SpringConfig.java
+++ src/main/java/com/example/demo/config/SpringConfig.java
@@ -1,13 +1,22 @@
 package com.example.demo.config;
 
+import com.example.demo.repository.JdbcMemberRepository;
 import com.example.demo.repository.MemberRepository;
-import com.example.demo.repository.MemoryMemberRepository;
 import com.example.demo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
