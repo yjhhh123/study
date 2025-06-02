9cdbeec812a042a29e8b3eeb73208f7099273f4d
yunjiho <yoonwlgh12@gmail.com>
2025-06-02 11:07:29 +0900

    spring 총 복습 브랜치 생성

---
+++ src/main/java/com/home/practice/spring_review_v1/SpringReviewV1Application.java
@@ -0,0 +1,13 @@
+package com.home.practice.spring_review_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReviewV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReviewV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/home/practice/spring_review_v1/SpringReviewV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.practice.spring_review_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReviewV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
