24b81e30d314ee8b51238dd5f722b1b993c91029
yunjiho <yoonwlgh12@gmail.com>
2025-06-12 15:22:49 +0900

    MySQL 연동 성공

---
+++ src/main/java/com/home/practice/spring_reviewv2_jpa/configuration/DBTestConfig.java
@@ -0,0 +1,25 @@
+package com.home.practice.spring_reviewv2_jpa.configuration;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@Configuration
+public class DBTestConfig {
+
+    @Bean
+    public CommandLineRunner testConnection(DataSource dataSource){
+        return args -> {
+            try (Connection conn = dataSource.getConnection()) {
+                System.out.println("MySQL 연결 성공!");
+                System.out.println("URL : " + conn.getMetaData().getURL());
+            } catch (Exception e) {
+                System.out.println("MySQL 연결 실패!");
+                e.printStackTrace();
+            }
+        };
+    }
+}
