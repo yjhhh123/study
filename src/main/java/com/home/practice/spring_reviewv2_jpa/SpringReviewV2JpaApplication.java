fe3138cca21a10639d92fe59aa1bf6f4197b41d7
yunjiho <yoonwlgh12@gmail.com>
2025-06-12 15:02:55 +0900

    프로젝트 생성

---
+++ src/main/java/com/home/practice/spring_reviewv2_jpa/SpringReviewV2JpaApplication.java
@@ -0,0 +1,13 @@
+package com.home.practice.spring_reviewv2_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReviewV2JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReviewV2JpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/home/practice/spring_reviewv2_jpa/SpringReviewV2JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.practice.spring_reviewv2_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReviewV2JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
